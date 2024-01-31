5dc4700616e761ef84f4702147ad2ef76f369ed7
TheuZCoder <matheusrdsilva1@gmail.com>
2024-01-31 16:04:24 -0300

    att

---
+++ meu_primeiro_programa.py
@@ -0,0 +1,15 @@
+print("hello , word!!!!")
+
+#Definindo informações
+
+nome = 'Fernanda'
+idade = 20
+
+#Imprimindo informações
+
+print(f'Olá, {nome}! Você tem {idade} anos')
+
+#realizando operações
+
+idade_no_futuro = idade + 5
+print(f'Daqui a 5 anos, você tera {idade_no_futuro}')
